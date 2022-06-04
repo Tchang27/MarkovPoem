0a44096e89095ec6f2fc5124239dab92aeeb819a
Thomas Chang <seowo1@MacBook-Pro-4.home>
2022-06-04 12:09:58 -0400

    cleaned up generate

--- README.txt
+++ README.txt
@@ -16,8 +16,8 @@ Known Issues/Bugs:
 Grammar rules are still being ironed out, as the generated lines
 usually output nonsensical words around half the time
 
-Working on comma placement, uncomment out code in write_line
-to test out functionality (still having issues)
+Working on comma placement, still having issues with commas
+in relation to independent/dependent clauses
 
 Libraries Needed:
 requests
--- generate.py
+++ generate.py
@@ -27,11 +27,6 @@ def generate_graph(words):
 
 def write_line(graph: Graph, words, count = 10):
     line = []
-    contains_verb = False
-    contains_subject = False
-    contains_object = False
-    complete_clause = False
-    determiner = False
 
     #for the inputted number of words, generate a line using graph
     #try to follow grammatical rules - commas for clauses, ending on non-preposition or conjunction
@@ -53,48 +48,56 @@ def write_line(graph: Graph, words, count = 10):
         line.append(word.value)
         last_word = word.value
         word = graph.get_next_word(word)
-
-    # line = nltk.pos_tag(line)
-
-    # for i in range(len(line)):
-    #     item = list(line[i])
-    #     if item[1] == 'DT' or item[1] == 'PRP$' or item[1] == 'PDT' :
-    #         determiner = True
-    #     if item[1][0] == 'V':
-    #         if not contains_verb:
-    #             contains_verb = True
-    #     if item[1][0] == 'N' or item[1] == 'PRP':
-    #         if not contains_subject:
-    #             contains_subject = True
-    #         elif not contains_object:
-    #             contains_object = True
-    #         elif not determiner:
-    #             print("here" + str(i))
-    #             prev_item = list(line[i-1])
-    #             prev_item[0] = str(prev_item[0]) + ','
-    #             line[i-1] = (prev_item[0], prev_item[1])
-    #             contains_verb = False
-    #             contains_subject = False
-    #             contains_object = False
-    #     if contains_verb and contains_subject and contains_object:
-    #         complete_clause = True
-    #         contains_verb = False
-    #         contains_subject = False
-    #         contains_object = False
-    #     if complete_clause:
-    #         item[0] = str(item[0]) + ','
-    #         line[i] = (item[0], item[1])
-    #         contains_verb = False
-    #         contains_subject = False
-    #         contains_object = False
-    #         determiner = False
-    #         complete_clause = False
-    
-    # corrected_line = []
-    # for item in line:
-    #     corrected_line.append(item[0])
-
     return line
+
+def clean_line(line):
+    contains_verb = False
+    contains_subject = False
+    contains_object = False
+    complete_clause = False
+    determiner = False
+
+    line = nltk.pos_tag(line)
+
+    for i in range(len(line)):
+        item = list(line[i])
+        if item[1] == 'DT' or item[1] == 'PRP$' or item[1] == 'PDT' :
+            determiner = True
+        if item[1][0] == 'V':
+            if not contains_verb:
+                contains_verb = True
+        if item[1][0] == 'N' or item[1] == 'PRP':
+            if not contains_subject:
+                contains_subject = True
+            elif not contains_object:
+                contains_object = True
+            elif not determiner:
+                print("here" + str(i))
+                prev_item = list(line[i-1])
+                prev_item[0] = str(prev_item[0]) + ','
+                line[i-1] = (prev_item[0], prev_item[1])
+                contains_verb = False
+                contains_subject = False
+                contains_object = False
+        if contains_verb and contains_subject and contains_object:
+            complete_clause = True
+            contains_verb = False
+            contains_subject = False
+            contains_object = False
+        if complete_clause:
+            item[0] = str(item[0]) + ','
+            line[i] = (item[0], item[1])
+            contains_verb = False
+            contains_subject = False
+            contains_object = False
+            determiner = False
+            complete_clause = False
+    
+    corrected_line = []
+    for item in line:
+        corrected_line.append(item[0])
+
+    return corrected_line
 
 
 def main(length):
@@ -108,9 +111,10 @@ def main(length):
 
     #use graph to generate a line, input miminmum word count
     generated_line = write_line(graph, words, length)
+    punct_line = clean_line(generated_line)
     
     #print line
-    return ' '.join(generated_line)
+    return ' '.join(punct_line)
 
 if __name__ == '__main__':
-    print(main(8))
+    print(main(12))
