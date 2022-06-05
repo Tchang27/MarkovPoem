1f2a1bb09f2c0489ef4690ffd013b3f2424bf0ee
Thomas Chang <seowo1@MacBook-Pro-4.home>
2022-06-05 11:44:47 -0400

    created generator object, to decrease runtime. Added more grammar rules

--- generate.py
+++ generate.py
@@ -1,4 +1,4 @@
-from distutils.command.clean import clean
+from multiprocessing.dummy import current_process
 from scrape import Scraper
 from graph import Graph, Vertex
 import random
@@ -6,137 +6,171 @@ import nltk
 from nltk.corpus import stopwords
 import re
 import string
+import time
 
 STOP_WORDS = set(stopwords.words('english'))
 
 #function if i need to grab words from a text document instead of scraping website
-def grab_words(path):
-    with open(path, 'r') as f:
-        text = f.read()
+class Generator:
+    def __init__(self):
+        #for scraping
+        # scraper = Scraper()
+        # words = scraper.scrape_poems('https://www.gutenberg.org/files/12242/12242-h/12242-h.htm')
 
-        #removes punctuation
-        text = re.sub(r'\[(.+)\]', ' ', text)
-        #removes roman numerals
-        text = re.sub(r'(?=\b[MCDXLVI]{1,6}\b)M{0,4}(?:CM|CD|D?C{0,3})(?:XC|XL|L?X{0,3})(?:IX|IV|V?I{0,3})(.)', ' ', text)
-        #removes hyphens
-        text = re.sub(r'[—]', ' ', text)
-        text = ' '.join(text.split())
-        text = text.lower()
-        text = text.translate(str.maketrans('','', string.punctuation))
+        #for text document
+        self.words = []
+        self.words = self.grab_words('dickinson_poems.txt')
 
-    words = text.split()
-    return words
+        #make graph
+        self.graph = self.generate_graph(self.words)
 
-def generate_graph(words):
-    g = Graph()
+    def grab_words(self,path):
+        with open(path, 'r') as f:
+            text = f.read()
 
-    previous_vertex = None
+            #removes punctuation
+            text = re.sub(r'\[(.+)\]', ' ', text)
+            #removes roman numerals
+            text = re.sub(r'(?=\b[MCDXLVI]{1,6}\b)M{0,4}(?:CM|CD|D?C{0,3})(?:XC|XL|L?X{0,3})(?:IX|IV|V?I{0,3})(.)', ' ', text)
+            #removes hyphens
+            text = re.sub(r'[—]', ' ', text)
+            text = ' '.join(text.split())
+            text = text.lower()
+            text = text.translate(str.maketrans('','', string.punctuation))
 
-    for word in words:
-        word_vertex = g.get_vertex(word)
-        if previous_vertex:
-            #idea: give less weight when connecting nouns
-            previous_vertex.increment_edge(word_vertex)
-        previous_vertex = word_vertex
+        words = text.split()
 
-    #after every word is in the graph, generate probability mappings
-    g.generate_probability_maps()
+        return words
 
-    return g
+    def generate_graph(self,words):
+        g = Graph()
+        words = nltk.pos_tag(words)
 
-def write_line(graph: Graph, words, count = 10):
-    line = []
+        previous_vertex = None
+        previous_pos = None
+        current_pos = None
 
-    #for the inputted number of words, generate a line using graph
-    #try to follow grammatical rules - commas for clauses, ending on non-preposition or conjunction
-    word = 'END'
-    while word == 'END':
-        word = graph.get_vertex(random.choice(words))
-    
-    for _ in range(count):
-        while word.value == 'END':
+        for word in words:
+            word = list(word)
+            word_vertex = g.get_vertex(word[0])
+            current_pos = word[1]
+
+            #change chain weighting based on previous part of speech
+            if previous_vertex:
+                    if previous_pos == current_pos:
+                        if previous_pos == 'CC':
+                            previous_vertex.increment_edge(word_vertex, 0.1)
+                        elif previous_pos[0] == 'N':
+                            previous_vertex.increment_edge(word_vertex, 0.3)
+                        elif previous_pos == 'PRP' or previous_pos == 'PRP$':
+                            previous_vertex.increment_edge(word_vertex, 0.1)
+                        else:
+                            previous_vertex.increment_edge(word_vertex)
+                    else:
+                        previous_vertex.increment_edge(word_vertex)
+
+            previous_vertex = word_vertex
+            previous_pos = current_pos
+
+        #after every word is in the graph, generate probability mappings
+        g.generate_probability_maps()
+
+        return g
+
+    def write_line(self, graph: Graph, words, count = 10):
+        line = []
+
+        #for the inputted number of words, generate a line using graph
+        #try to follow grammatical rules - commas for clauses, ending on non-preposition or conjunction
+        word = 'END'
+        while word == 'END':
+            word = graph.get_vertex(random.choice(words))
+        
+        prev_word = None
+        for _ in range(count):
+            while word.value == 'END' or word.value == prev_word:
+                word = graph.get_next_word(word)
+            line.append(word.value)
+            prev_word = word.value
             word = graph.get_next_word(word)
-        line.append(word.value)
-        word = graph.get_next_word(word)
-    
-    last_word = line[len(line)-1]
-    word = graph.get_next_word(graph.vertices[last_word])
-    while(last_word in STOP_WORDS):
-        while word.value == 'END':
+        
+        last_word = line[len(line)-1]
+        word = graph.get_next_word(graph.vertices[last_word])
+        while(last_word in STOP_WORDS):
+            while word.value == 'END':
+                word = graph.get_next_word(word)
+            line.append(word.value)
+            last_word = word.value
             word = graph.get_next_word(word)
-        line.append(word.value)
-        last_word = word.value
-        word = graph.get_next_word(word)
-    return line
+        return line
 
-def clean_line(line):
-    contains_verb = False
-    contains_subject = False
-    contains_object = False
-    complete_clause = False
-    determiner = False
+    def clean_line(self, line):
+        contains_verb = False
+        contains_subject = False
+        contains_object = False
+        complete_clause = False
+        determiner = False
+        previous_pos = None
 
-    line = nltk.pos_tag(line)
+        line = nltk.pos_tag(line)
 
-    for i in range(len(line)):
-        item = list(line[i])
-        if item[1] == 'DT' or item[1] == 'PRP$' or item[1] == 'PDT' :
-            determiner = True
-        if item[1][0] == 'V':
-            if not contains_verb:
-                contains_verb = True
-        if item[1][0] == 'N' or item[1] == 'PRP':
-            if not contains_subject:
-                contains_subject = True
-            elif not contains_object:
-                contains_object = True
-            elif not determiner:
-                prev_item = list(line[i-1])
-                prev_item[0] = str(prev_item[0]) + ','
-                line[i-1] = (prev_item[0], prev_item[1])
+        for i in range(len(line)):
+            item = list(line[i])
+
+            if item[1] == 'DT' or item[1] == 'PRP$' or item[1] == 'PDT' :
+                determiner = True
+            elif item[1][0] == 'V':
+                if not contains_verb:
+                    contains_verb = True
+            elif item[1][0] == 'N' or item[1] == 'PRP':
+                if not contains_subject:
+                    contains_subject = True
+                elif not contains_object:
+                    contains_object = True
+                # elif previous_pos != 'DETERMINER':
+                #     prev_item = list(line[i-1])
+                #     prev_item[0] = str(prev_item[0]) + ','
+                #     line[i-1] = (prev_item[0], prev_item[1])
+                #     contains_verb = False
+                #     contains_subject = False
+                #     contains_object = False
+            if contains_verb and contains_subject and contains_object and previous_pos != 'DETERMINER' and previous_pos != 'DT':
+                complete_clause = True
                 contains_verb = False
                 contains_subject = False
                 contains_object = False
-        if contains_verb and contains_subject and contains_object:
-            complete_clause = True
-            contains_verb = False
-            contains_subject = False
-            contains_object = False
-        if complete_clause:
-            item[0] = str(item[0]) + ','
-            line[i] = (item[0], item[1])
-            contains_verb = False
-            contains_subject = False
-            contains_object = False
-            determiner = False
-            complete_clause = False
-    
-    corrected_line = []
-    for item in line:
-        corrected_line.append(item[0])
+            if complete_clause:
+                item[0] = str(item[0]) + ','
+                line[i] = (item[0], item[1])
+                contains_verb = False
+                contains_subject = False
+                contains_object = False
+                determiner = False
+                complete_clause = False
+            if determiner:
+                previous_pos = 'DETERMINER' 
+                determiner = False
+            else:
+                previous_pos = item[1] 
+        
+        corrected_line = []
+        for item in line:
+            corrected_line.append(item[0])
 
-    return corrected_line
+        return corrected_line
 
 
-def main(length):
-    #for scraping
-    # scraper = Scraper()
-    # words = scraper.scrape_poems('https://www.gutenberg.org/files/12242/12242-h/12242-h.htm')
+    def generate(self, length):
+        #use graph to generate a line, input miminmum word count
+        generated_line = self.write_line(self.graph, self.words, length)
 
-    #for text document
-    words = []
-    words = grab_words('dickinson_poems.txt')
+        #this cleans the line using my own grammar rules - STILL INCOMPLETE/HAS BUGS
+        punct_line = self.clean_line(generated_line)
 
-    #make graph
-    graph = generate_graph(words)
-
-    #use graph to generate a line, input miminmum word count
-    generated_line = write_line(graph, words, length)
-    #this cleans the line using my own grammar rules - STILL INCOMPLETE/HAS BUGS
-    punct_line = clean_line(generated_line)
-    
-    #print line
-    return ' '.join(punct_line)
+        
+        #print line
+        return ' '.join(punct_line)
 
 if __name__ == '__main__':
-    print(main(10))
+    generator = Generator()
+    print(generator.generate(10))
--- writer.py
+++ writer.py
@@ -1,25 +1,26 @@
-from generate import main
+from generate import Generator
 import time
 
-def write_lines_to_file(path, num):
+def write_lines_to_file(g, path, num):
     f = open(path, 'w')
 
     for _ in range(num):
-        f.write(main(12) + "\n")
+        f.write(g.generate(12) + "\n")
 
     f.close()
 
-def write_paragraph(path, num):
+def write_paragraph(g, path, num):
     f = open(path, 'w')
 
-    f.write(main(num) + "\n")
+    f.write(g.generate(num) + "\n")
 
     f.close()
 
 if __name__ == '__main__':
     start_time = time.time()
-    write_lines_to_file('generate_lines.txt', 100)
-    #write_paragraph('generate_lines.txt', 100)
+    generator = Generator()
+    write_lines_to_file(generator, 'generate_lines.txt', 100)
+    #write_paragraph(generator, 'generate_lines.txt', 100)
     end_time = time.time()
     print("Time to execute: " + str(end_time-start_time))
 
